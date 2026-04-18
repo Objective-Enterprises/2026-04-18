756f633653153b471f9b2e916026ee6cf3c8f370
David Y. Stephenson <david@davidystephenson.com>
2026-04-18 11:52:50 -0400

    Define service and controllers

--- src/controllers/subredditController.js
+++ src/controllers/subredditController.js
@@ -5,13 +5,76 @@ import {
 } from "../services/subredditService.js";
 
 export const getAllSubreddits = async (req, res) => {
-  // YOUR CODE HERE
+  try {
+    const subreddits = await fetchAllSubreddits()
+    if (subreddits.length === 0) {
+      const output = { success: false, message: 'No subreddits found' }
+      res.status(404).json(output)
+    }
+    const output = { success: true, data: subreddits, message: 'Subreddits found' }
+    res.json(output)
+  } catch (error) {
+    const output = { success: false, message: 'Error finding subreddits' }
+    res.status(500).json(output)
+  }
 };
 
 export const createSubreddit = async (req, res) => {
-  // YOUR CODE HERE
+  try {
+    if (!req.body.name) {
+      return res.status(400).json({
+        success: false,
+        message: 'Name missing'
+      })
+    }
+    if (!req.body.author) {
+      return res.status(400).json({
+        success: false,
+        message: 'Author missing'
+      })
+    }
+    const subreddit = await createNewSubreddit(
+      req.body.name,
+      req.body.description,
+      req.body.author
+    )
+    if (!subreddit) {
+      return res.status(409).json({
+        success: false,
+        message: 'Name taken'
+      })
+    }
+    res.status(201).json({
+      success: true,
+      data: subreddit,
+      message: "Subreddit created"
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Error creating subreddit'
+    })
+  }
 };
 
 export const getSubredditWithThreads = async (req, res) => {
-  // YOUR CODE HERE
+  try {
+    const subreddit = await fetchSubredditWithThreads(req.params.id)
+    if (!subreddit) {
+      return res.status(404).json({
+        success: false,
+        message: 'Subreddit not found'
+      })
+    }
+    res.json({
+      success: true,
+      data: subreddit,
+      message: 'Subreddit found'
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error getting subreddit"
+    })
+  }
 };
--- src/services/subredditService.js
+++ src/services/subredditService.js
@@ -2,13 +2,36 @@ import Subreddit from "../models/Subreddit.js";
 import Thread from "../models/Thread.js";
 
 export const fetchAllSubreddits = async () => {
-  // YOUR CODE HERE
+  const subreddits = await Subreddit.find()
+  return subreddits
 };
 
 export const createNewSubreddit = async (name, description, author) => {
-  // YOUR CODE HERE
+  const existingSubreddit = await Subreddit.findOne({ name })
+  if (existingSubreddit) {
+    return
+  }
+  const data = { name, description, author }
+
+  // v1
+  // const subreddit = await Subreddit.create(data)
+
+  // v2 
+  const subreddit = new Subreddit(data)
+  await subreddit.save()
+
+  return subreddit
 };
 
 export const fetchSubredditWithThreads = async (id) => {
-  // YOUR CODE HERE
+  const subreddit = await Subreddit.findById(id)
+  if (!subreddit) {
+    return
+  }
+
+  const threads = await Thread
+    .find({ subreddit: id })
+    .populate('author')
+
+  return { subreddit, threads }
 };
